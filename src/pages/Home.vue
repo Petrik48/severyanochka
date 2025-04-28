bc91855aee633855a00395e1d8d0ef517543c0a9
petrik48 <petrov.avl@npc-ksb.ru>
2025-04-28 16:55:59 +0300

    1 46:00

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,6 +4,35 @@ import { Typography } from '../shared/typography'
 
 <template>
   <main>
-    <Typography tag="p"> test </Typography>
+    <div class="flex gap-4">
+      <div class="flex flex-col gap-2 justify-between">
+        <Typography tag="p" size="xs"> test </Typography>
+        <Typography tag="p" size="s"> test </Typography>
+        <Typography tag="p" size="m"> test </Typography>
+        <Typography tag="p" size="l"> test </Typography>
+        <Typography tag="p" size="xl"> test </Typography>
+      </div>
+      <div class="flex flex-col gap-2 justify-between">
+        <Typography tag="p" size="xs" bold> test </Typography>
+        <Typography tag="p" size="s" bold> test </Typography>
+        <Typography tag="p" size="m" bold> test </Typography>
+        <Typography tag="p" size="l" bold> test </Typography>
+        <Typography tag="p" size="xl" bold> test </Typography>
+      </div>
+      <div class="flex flex-col gap-2 justify-between">
+        <Typography tag="h1" size="xs"> test </Typography>
+        <Typography tag="h2" size="s"> test </Typography>
+        <Typography tag="h3" size="m"> test </Typography>
+        <Typography tag="h4" size="l"> test </Typography>
+        <Typography tag="h5" size="xl"> test </Typography>
+      </div>
+      <div class="flex flex-col gap-2 justify-between">
+        <Typography tag="h1" size="xs" bold> test </Typography>
+        <Typography tag="h2" size="s" bold> test </Typography>
+        <Typography tag="h3" size="m" bold> test </Typography>
+        <Typography tag="h4" size="l" bold> test </Typography>
+        <Typography tag="h5" size="xl" bold> test </Typography>
+      </div>
+    </div>
   </main>
 </template>
--- src/shared/typography/Typography.vue
+++ src/shared/typography/Typography.vue
@@ -1,13 +1,47 @@
 <script lang="ts" setup>
 interface Props {
   tag: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  size: 'xs' | 's' | 'm' | 'l' | 'xl'
+  bold?: boolean
 }
 
-const { tag } = defineProps<Props>()
+const { tag, size, bold } = defineProps<Props>()
 </script>
 
 <template>
-  <component :is="tag">
+  <component
+    :is="tag"
+    :class="['typography', `typography--${size}`, `${bold ? 'typography--bold ' : ''}`]"
+  >
     <slot></slot>
   </component>
 </template>
+
+<style lang="scss" scoped>
+.typography {
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 150%;
+  color: var(--main-onsurface);
+
+  &--xs {
+    font-size: 12px;
+  }
+  &--s {
+    font-size: 16px;
+  }
+  &--m {
+    font-size: 18px;
+  }
+  &--l {
+    font-size: 24px;
+  }
+  &--xl {
+    font-size: 36px;
+  }
+
+  &--bold {
+    font-weight: bold;
+  }
+}
+</style>
