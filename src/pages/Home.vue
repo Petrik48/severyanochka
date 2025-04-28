a5b0096aa83fa75e509b865ce919b439db765e7b
Petrik48 <alex.petrov.15@mail.ru>
2025-04-28 06:18:17 +0300

    1 39:00

---
+++ src/pages/Home.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Typography } from '../shared/typography'
+</script>
+
+<template>
+  <main>
+    <Typography tag="p"> test </Typography>
+  </main>
+</template>
---
+++ src/shared/typography/Typography.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+interface Props {
+  tag: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}
+
+const { tag } = defineProps<Props>()
+</script>
+
+<template>
+  <component :is="tag">
+    <slot></slot>
+  </component>
+</template>
